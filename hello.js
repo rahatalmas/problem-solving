ac8fcd3e83fde216d93df7d38ece6015121baa3c
rahatalmas <rahat.almas5599@gmail.com>
2023-05-26 03:45:48 +0600

    sad

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 const mars = () =>{
     console.log("hello world\n");
     console.log("I am in love with someone from diu pharmachy department...")
+    console.log("I want to explain my feelings...")
 }
 
 mars();
