305a55a49b6c52060fbb8bb466a41eb66b0223f1
rahatalmas <rahat.almas5599@gmail.com>
2023-05-29 07:50:39 +0600

    __proto__1

--- JavaScript/__proto__/__proto__.js
+++ JavaScript/__proto__/__proto__.js
@@ -1,7 +1,45 @@
 //javascript prototypes...
+//prototypal inheritance and the prototype chain
+//-> es6 introduced classes which is the modern way to construct objects
+// That said, prototypal inheritance and the prototype chain are:
+//   -> "under the hood", (es6 classes are considered "syntactic sugar")
+//   -> often in interview question
+//   -> and are useful to understand.
 
-function love(){
-    console.log("hello badhon will you marry me ? ");
+// object literals
+const person = {
+    alive: true
 }
 
-love();
+const musician = {
+    plays : true
+}
+
+console.log(person.alive) // outputs true
+console.log(musician.alive) // outputs undefined
+
+musician.__proto__ = person;
+
+console.log(person.alive) //output true
+console.log(musician.alive) //output true
+
+// musician.__proto__ = person 
+// this is inhertance. Now musician is inherited from person. 
+// person is the parent class of musician or the proto of musician
+
+
+// js now has getPrototypeOf and setPrototypeOf methods instead of using 
+// __proto__ 
+
+Object.getPrototypeOf(musician,person); //setting prototype
+console.log(Object.getPrototypeOf(musician)); 
+console.log(musician.__proto__);
+console.log(Object.getPrototypeOf(musician) == musician.__proto__) // this proves previous two ways does the same thing.
+
+//  Extending the prototype chain => general to specific to more specific
+
+const guitar ={
+    strings: 6,
+    __proto__: musician
+}
+
