e43577cb71433f9a63acd9998b84dc79b6172304
rahatalmas <rahat.almas5599@gmail.com>
2023-06-03 00:53:04 +0600

    prisma

--- hello-prisma/index.js
+++ hello-prisma/index.js
@@ -3,13 +3,17 @@ const cors = require('cors');
 const PORT = 8080;
 
 const app = express();
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors());
 
 app.get("/",(req,res)=>{
     res.send("hello world");
 })
-
+app.post("/register",(req,res,next)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
 app.listen(PORT,()=>{
     console.log("server running...")
 })
