049bbd069d7c70d83f3e33f9b5326a1eeb45616d
rahatalmas <rahat.almas5599@gmail.com>
2023-06-28 04:01:13 +0600

    note

--- JavaScript/sp.js
+++ JavaScript/sp.js
@@ -1,3 +1,17 @@
 let a = [1,2,3,4,5];
 let b = [...a,6];
-console.log(b);
+console.log(a,b);
+
+let c = [a,b]
+let d = [...c];
+//console.log(d);
+let e = [a,c];
+console.log(e[1][0][0])
+
+function f(){
+    z=5;
+    console.log(z);
+}
+f();
+
+console.log(typeof(f));
