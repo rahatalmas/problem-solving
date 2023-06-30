7fc9fe0b281b3205e16451e41f0d159fc9f870ad
rahatalmas <rahat.almas5599@gmail.com>
2023-06-30 18:25:54 +0600

    destructor

---
+++ JavaScript/es6/dest.js
@@ -0,0 +1,10 @@
+const obj = {
+    name:'rahat almas',
+    crush_name:'Rabia Badhon',
+    dept:'cse',
+    her_dept:'pharmachy'
+}
+
+let {name,crush_name} = obj;
+console.log(name, crush_name);
+
