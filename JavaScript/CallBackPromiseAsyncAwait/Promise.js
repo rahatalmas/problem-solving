b5de48aad3bb7bae80b7258bda679d1359f648ae
rahatalmas <rahat.almas5599@gmail.com>
2023-07-06 00:00:18 +0600

    promiseClear

--- JavaScript/CallBackPromiseAsyncAwait/Promise.js
+++ JavaScript/CallBackPromiseAsyncAwait/Promise.js
@@ -1,8 +1,10 @@
 const getData = async (url) =>{
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
-    //return data;
+    return data;
 }
 
 const d = getData("https://jsonplaceholder.typicode.com/posts");
+d.then(a=>{
+  console.log(a);
+})
