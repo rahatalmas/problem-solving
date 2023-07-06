159584bd336770238ddf1a373cab30207fab1503
rahatalmas <rahat.almas5599@gmail.com>
2023-07-06 19:11:43 +0600

    cookie

--- JavaScript/CallBackPromiseAsyncAwait/tempCodeRunnerFile.js
+++ JavaScript/CallBackPromiseAsyncAwait/tempCodeRunnerFile.js
@@ -1,1 +1,10 @@
-console.log(data);
+const d = {type:"pie"}
+d.type = "a";
+
+let f =() =>{
+    console.log(a);
+    var a = 5;
+}
+f();
+f();
+console.log(typeof(12.4))
--- NodePractice/index.js
+++ NodePractice/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 //require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 
+app.get("/",(req,res)=>{
+    res.setHeader('set-cookies',['name=howl;HttpOnly:true','movie=howls moving castles']);
+    //res.cookie('name','rahat',{httpOnly:true});
+    res.send(req.headers);
+})
 
 app.listen(8080,(req,res)=>{
     console.log("server running...");
