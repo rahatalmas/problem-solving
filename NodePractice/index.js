a60d3c2b4e7dc8f333d6f0ae2ee47fec99c4f9da
rahatalmas <rahat.almas5599@gmail.com>
2023-07-08 23:18:19 +0600

    fileUpload

--- NodePractice/index.js
+++ NodePractice/index.js
@@ -6,22 +6,27 @@ const cors = require('cors');
 const multer = require('multer');
 const app = express();
 const routes = require('./routes/routes.js');
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-//app.use(cookieParser());
-//app.use(bodyParser.json({}));
+//app.use(express.json());
+//app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(bodyParser.json({}));
 // for parsing application/xwww-form-urlencoded
 //app.use(bodyParser.urlencoded({extended: false})); //limit: "50mb",
 app.use(cors());
 //res.setHeader('set-cookies','name=howl');
-//es.cookie('name','howl',{httpOnly:true});
+//es.cookie('name','howl',{httpOnly:true}
+
+const upload = multer({dest:'public'});
+
 app.use("/",(req,res,next)=>{
     console.log("i watch everything...");
     next();
 })
 app.use('/files',express.static(path.join(__dirname,'public')));
 app.use('/images',express.static(path.join(__dirname,'public/images')));
-
+app.post("/fileUpload",upload.single('file'),(req,res)=>{
+    console.log(req.file);
+})
 app.use("/",routes);
 
 app.listen(8000,(req,res)=>{
