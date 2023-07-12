3b6d7caa3c645fed8234235667ddb6bc3ef9821a
rahatalmas <rahat.almas5599@gmail.com>
2023-07-13 05:20:44 +0600

    loader

--- ReactPractice/my-practicepad/src/loaderFunctions/LoaderFunctions.js
+++ ReactPractice/my-practicepad/src/loaderFunctions/LoaderFunctions.js
@@ -1,12 +1,13 @@
-export const TodoLoader = async () =>{
+export const TodoLoader = async (req) =>{
    try{
       const data = await fetch("https://jsonplaceholder.typicode.com/todos");
       const todos = await data.json();
-      console.log(todos)
+      console.log(todos);
       return todos;
    }
    catch(err){
       return err.message;
    }
-   //return todos;
 }
+
+
