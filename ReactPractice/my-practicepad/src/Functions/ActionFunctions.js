8aa9d974c079a45c656bc7c2288b89577dc7572c
rahatalmas <rahat.almas5599@gmail.com>
2023-07-15 18:31:19 +0600

    postData

--- ReactPractice/my-practicepad/src/Functions/ActionFunctions.js
+++ ReactPractice/my-practicepad/src/Functions/ActionFunctions.js
@@ -1,12 +1,23 @@
-import {redirect} from 'react-router-dom';
+import {json, redirect} from 'react-router-dom';
 import axios from 'axios';
+//import { head } from '../../../../NodePractice/routes/routes';
 
 export const BlogPostAction = async ({request,params}) =>{
      let data = await request.formData();
-     console.log(typeof(data.JSON()));
-     axios.post("http://localhost:8000/post",JSON.stringify(data))
-     .then(result=>{
-          console.log(result.data);
-     })
+     axios.post("http://localhost:8000/post",
+     {
+       title:data.get('title'),
+       content:data.get('content')
+     }
+  )
+  .then(result=>{
+       console.log(result.data);
+  })
      return redirect('/blogs');
 }
+
+/*
+
+
+
+*/
