aaac7c808d7fb700aef9c510945bbc47fad0b22c
rahatalmas <rahat.almas5599@gmail.com>
2023-07-26 23:27:22 +0600

    arrayiteration

--- Languages/JavaScript/array/array.js
+++ Languages/JavaScript/array/array.js
@@ -5,6 +5,7 @@ for(let i=1;i<11;i++){
 console.log(arr);
 arr.push(11,12,13,14,15);
 console.log(arr);
+
 //method: 
 // push(...items) -> add items at the back of an array...
 arr.unshift('a','b','c');
@@ -16,6 +17,7 @@ console.log(arr);
 console.log("first item of the array: " ,arr.shift()); 
 //shift() -> extracts an item from the beginning . it also shows the value...
 console.log(arr);
+
 
 // method : splice(start[,deleteCount, elem1, ..., elemN])
 // it modifies arr from the index start, removes deleteCount elements and then also it can insert 
@@ -36,11 +38,11 @@ console.log("array:",arr);
 arr.splice(-5,0,"google","netflix");
 console.log("array",arr);
 
+
 //slice([start],[end]) -> it copies elements from an array start to end-1 index
 //negative indexing is also allowed
 //if we don't set end index the rest elements will copy from start index;
 //if we don't set start index and also end index the rest of the elements will get copied..
-
 let subArray = arr.slice(0,6);
 let subArray2 = arr.slice(5);
 console.log("without start and end slice():",arr.slice());
@@ -49,3 +51,9 @@ console.log("subArray2",subArray2);
 console.log("SubArray from end:", subArray.slice(-5));
 console.log("subArray negative indexing:",arr.slice(-5,-1));
 
+// iterations:
+//forEach()
+console.log("\nusing forEach() method: ");
+arr.forEach((element,index,arr)=>{
+    console.log(element);
+})
